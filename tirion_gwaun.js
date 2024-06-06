98ef8a452127e127edd0c14b234340ca6e3749d7
Henry GALVEZ <henry-externe.galvez@edf.fr>
2024-06-06 21:54:08 +0200

    fix(script): :recycle: Lint

--- tirion_gwaun.js
+++ tirion_gwaun.js
@@ -1,43 +1,26 @@
 // Web crawler
+import {list_servers} from 'onar.js';
+
 /** @param {NS} ns **/
-const home = 'home';
-
-const script = 'tirion_gwaun.js';
-
 export async function main(ns) {
     const serverDB = list_servers(ns).map((host)=>storeInfo(ns, host));
     localStorage.setItem('serverDB', JSON.stringify(serverDB));
+    self.console.log(`${serverDB.length} added`);
 }
 
 function storeInfo(ns, iAm) {
     const myData = {
         id: iAm,
-        growingTime : ns.getGrowTime(iAm),
-        hackingTime:ns.getHackTime(iAm),
-        weakenTime:ns.getWeakenTime(iAm),
+        growingTime: ns.getGrowTime(iAm),
+        hackingTime: ns.getHackTime(iAm),
+        weakenTime: ns.getWeakenTime(iAm),
         maxMoney: ns.getServerMaxMoney(iAm),
-        maxRam : ns.getServerMaxRam(iAm),
-        minSecurity:ns.getServerMinSecurityLevel(iAm),
-        portsRequired:ns.getServerNumPortsRequired(iAm),
-        requiredHacking:ns.getServerRequiredHackingLevel(iAm),
+        maxRam: ns.getServerMaxRam(iAm),
+        minSecurity: ns.getServerMinSecurityLevel(iAm),
+        portsRequired: ns.getServerNumPortsRequired(iAm),
+        requiredHacking: ns.getServerRequiredHackingLevel(iAm),
         hasRoot: ns.hasRootAccess(iAm),
+        task: 'weaken'
     };
     return myData;
-}
-
-function scan(ns, parent, server, list) {
-    const children = ns.scan(server);
-    for (let child of children) {
-        if (parent == child) {
-            continue;
-        }
-        list.push(child);
-        scan(ns, server, child, list);
-    }
-}
-
-function list_servers(ns) {
-    const list = [];
-    scan(ns, '', 'home', list);
-    return list;
 }
