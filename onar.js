c6498617030df9d79a0f3fad8e85afe1bd55ed01
Henry GALVEZ <henry-externe.galvez@edf.fr>
2024-06-06 21:59:42 +0200

    feat(script): :tada: Espacio para funciones generales

---
+++ onar.js
@@ -0,0 +1,21 @@
+// Armeria/Utilerias
+function scan(ns, parent, server, list) {
+    const children = ns.scan(server);
+    for (let child of children) {
+        if (parent == child) {
+            continue;
+        }
+        list.push(child);
+        scan(ns, server, child, list);
+    }
+}
+
+export function list_servers(ns) {
+    const list = [];
+    scan(ns, '', 'home', list);
+    return list;
+}
+
+export function iCanHack(ns, target) {
+    return ns.getHackingLevel() * 0.9 > target.requiredHacking;
+}
