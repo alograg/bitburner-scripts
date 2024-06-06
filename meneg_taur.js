8a62bdd016c0bf41f424d2f3b5c3de13e362e9f2
Henry GALVEZ <henry-externe.galvez@edf.fr>
2024-06-06 21:57:45 +0200

    fix(script): :tada: Optimizacion de busqueda y ataque

--- meneg_taur.js
+++ meneg_taur.js
@@ -1,20 +1,65 @@
 // Gusano
+import {iCanHack} from 'onar.js';
 const script = "nencilur.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const serverDB = JSON.parse(localStorage.getItem('serverDB'));
-    const swriptMem = ns.getScriptRam(script);
+    self.console.groupCollapsed('meneg_taur');
     ns.atExit(()=>{
         ns.exec("tirion_gwaun.js", 'home');
-    });
-    serverDB.reduce((result,item)=>{
+    }
+    );
+    let serverDB = JSON.parse(localStorage.getItem('serverDB'));
+    if (!serverDB)
+        ns.exit();
+    const scriptMem = ns.getScriptRam(script);
+    const notRootServer = serverDB.reduce((result,item)=>{
         if (!item.hasRoot) {
             result.push(item);
         }
         return result;
     }
-    , []).forEach((item)=>{
+    , []);
+    self.console.groupCollapsed('penetrate');
+    penetrateNodes(ns, serverDB);
+    ns.exec("tirion_gwaun.js", 'home');
+    self.console.groupEnd();
+    serverDB = JSON.parse(localStorage.getItem('serverDB'));
+    const rootedServer = serverDB.reduce((result,item)=>{
+        if (item.hasRoot) {
+            result.push(item);
+        }
+        return result;
+    }
+    , []);
+    self.console.log(rootedServer);
+    let forLog = [];
+    for (let i = rootedServer.length - 1; i >= 0; i--) {
+        let item = rootedServer[i];
+        let threads = Math.floor((item.maxRam - ns.getServerUsedRam(item.id)) / scriptMem);
+        if (threads >= 1) {
+            forLog.push({
+                host: item.id,
+                scp: ns.scp(script, item.id, 'home'),
+                exec: ns.exec(script, item.id, threads, item.id)
+            });
+        }
+    }
+    self.console.table(forLog);
+    self.console.groupEnd();
+}
+
+function penetrateNodes(ns, serverDB) {
+    const notRootServer = serverDB.reduce((result,item)=>{
+        if (!item.hasRoot) {
+            result.push(item);
+        }
+        return result;
+    }
+    , []);
+    notRootServer.forEach((item)=>{
+        if (iCanHack(ns, item))
+            return;
         if (ns.fileExists("HTTPWorm.exe", "home")) {
             ns.httpworm(item.id);
         }
@@ -29,21 +74,9 @@ export async function main(ns) {
         }
         try {
             ns.nuke(item.id);
-        }catch (e) {
-        }
-    }
-    );
-    serverDB.reduce((result,item)=>{
-        if (item.hasRoot) {
-            result.push(item);
-        }
-        return result;
-    }
-    , []).forEach((item)=>{
-        let threads = Math.floor((item.maxRam - ns.getServerUsedRam(item.id)) / swriptMem);
-        if (threads >= 1) {
-            ns.scp(script, item.id, 'home');
-            ns.exec(script, item.id, threads);
+            self.console.log(`${item.id} hacked`);
+        } catch (e) {
+            self.console.log(`${item.id} not hacked`);
         }
     }
     );
