68aaf12b2276167504e093be3a092124c66e8a82
Henry GALVEZ <henry-externe.galvez@edf.fr>
2024-06-06 21:58:25 +0200

    fix(script): :tada: Optimizacion offline

--- nencilur.js
+++ nencilur.js
@@ -1,20 +1,53 @@
 // Sanguijuela
+
+const targetJson = 'auleonto.txt';
+const actions = ['weaken', 'growing', 'hacking'];
+
 /** @param {NS} ns **/
 export async function main(ns) {
-    let hackTarget;
+    let hackTarget, lastAction, lastResult;
     while (true) {
-        hackTarget = JSON.parse(localStorage.getItem('hackTarget'));
+        let jsonTarget = localStorage.getItem('hackTarget');
+        if (jsonTarget) {
+            ns.rm(targetJson);
+            hackTarget = JSON.parse(localStorage.getItem('hackTarget'));
+            ns.write(targetJson, jsonTarget);
+        } else {
+            hackTarget = JSON.parse(ns.read(targetJson)) || {
+                id: ns.args[0]
+            };
+        }
+        if (!hackTarget.id) {
+            await ns.sleep(1000 * 5);
+            continue;
+        }
+        if (hackTarget.id == ns.args[0] || !jsonTarget) {
+            //hackTarget.task = actions.sort( () => .5 - Math.random() )[];
+            if (!lastResult)
+                switch (lastAction) {
+                case 'growing':
+                    hackTarget.task = 'hacking';
+                    break;
+                case 'hacking':
+                    hackTarget.task = 'weaken';
+                    break;
+                case 'weaken':
+                default:
+                    hackTarget.task = actions.filter((i)=>(i != lastAction)).sort(()=>(0.5 - Math.random()))[0];
+                    break;
+                }
+        }
         switch (hackTarget.task) {
         case 'weaken':
-            await ns.weaken(hackTarget.id);
+            lastResult = await ns.weaken(hackTarget.id);
             break;
         case 'growing':
-            await ns.grow(hackTarget.id);
+            lastResult = await ns.grow(hackTarget.id);
             break;
         case 'hacking':
-            await ns.hack(hackTarget.id);
+            lastResult = await ns.hack(hackTarget.id);
             break;
         }
-        await ns.sleep(hackTarget[`${hackTarget.task}Time`] * 1.1);
+        lastAction = hackTarget.task;
     }
 }
